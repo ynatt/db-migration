6caf6814a063003381c065c891b39c46b495e436
zhv <zhslma@gmail.com>
2017-07-09 20:55:46 +0400

    * improved test with mockito * removing unused files

--- db-migration-provider/src/test/java/TestTrackingStatement.java
+++ db-migration-provider/src/test/java/TestTrackingStatement.java
@@ -1,4 +1,3 @@
-import db.migration.model.Table;
 import db.migration.model.modification.drop.DropTable;
 import db.migration.provider.SQLiteExecutor;
 import db.migration.provider.SQLiteQueryParserManager;
@@ -7,30 +6,64 @@ import db.migration.service.DBState;
 import db.migration.service.TrackingConnection;
 import db.migration.service.TrackingDataSource;
 import db.migration.service.TrackingStatement;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.sql.SQLException;
-import static org.junit.Assert.assertEquals;
 
 public class TestTrackingStatement {
-    private String dataSourceUrl ="jdbc:sqlite:test.db";
+    private String dataSourceUrl ="jdbc:sqlite:target/test.db";
 
-    private DBState dbState = new DBState("Test State",new SQLiteExecutor());
+    private TrackingDataSource trackingDataSource;
 
-    private TrackingDataSource trackingDataSource = new SQLiteTrackingDataSource(dataSourceUrl);
-
+    @Before
+    public void setUp() throws Exception {
+        trackingDataSource = new SQLiteTrackingDataSource(dataSourceUrl);
+    }
 
     @Test
     public void trackingStatementTest() throws SQLException {
-        TrackingStatement trackingStatement = getTrackingStatement();
-        trackingStatement.execute("drop table if exists users");
-        DropTable expectedDropTable = new DropTable(new Table("users"));
-        expectedDropTable.setIfExists(true);
-        assertEquals(expectedDropTable,dbState.getChanges().get(0));
+        DBState dbState =
+                Mockito.spy(new DBState("Test State",new SQLiteExecutor()));
+
+        TrackingConnection trackingConnection = trackingDataSource.getTrackingConnection(dbState);
+        TrackingStatement statement = trackingConnection.createTrackingStatement(new SQLiteQueryParserManager());
+        statement.execute("drop table if exists users");
+
+        Mockito.verify(dbState).addChange(Mockito.isA(DropTable.class));
+
+//        DropTable expectedDropTable = new DropTable(new Table("users"));
+//        expectedDropTable.setIfExists(true);
+//        assertEquals(expectedDropTable, this.dbState.getChanges().get(0));
     }
 
-    private TrackingStatement getTrackingStatement() throws SQLException {
+    @Test
+    public void testExportToXml() throws Exception {
+        DBState dbState = new DBState("Test State",new SQLiteExecutor());
+
         TrackingConnection trackingConnection = trackingDataSource.getTrackingConnection(dbState);
-        return trackingConnection.createTrackingStatement(new SQLiteQueryParserManager());
+        TrackingStatement statement = trackingConnection.createTrackingStatement(new SQLiteQueryParserManager());
+        statement.execute("drop table if exists users");
+        statement.execute("create table t1 (a int)");
+
+        dbState.exportChanges("target/export.xml");
+    }
+
+    @Test
+    public void testImportToXml() throws Exception {
+        DBState dbState = new DBState("Test State",new SQLiteExecutor());
+
+        TrackingConnection trackingConnection = trackingDataSource.getTrackingConnection(dbState);
+        TrackingStatement statement = trackingConnection.createTrackingStatement(new SQLiteQueryParserManager());
+        statement.execute("drop table if exists users");
+
+        dbState.exportChanges("target/export2.xml");
+
+        dbState.getChanges().clear();
+
+        dbState.importChanges("target/export2.xml");
+
+        System.out.println(dbState.getChanges().get(0).getChangeType());
     }
 }
--- site/index.md
+++
@@ -1,9 +0,0 @@
-# Model (ok)
-
-# Service
-
-# Tracking
-// TODO: check JDBC 4.0 spec
-https://jcp.org/aboutJava/communityprocess/final/jsr221/index.html
-
-# Import/Export
